f0d64666ce9ce3bde7221ac9247be0041c0fa1d1
notkrithiramnath <krithiramnath@gmail.com>
2025-12-25 17:22:10 -0500

    fixed python errors

--- python/rec_eng.py
+++ python/rec_eng.py
@@ -1,9 +1,7 @@
 from flask import Flask, request, jsonify
 # Flask is web framework to create the python server
 import requests
-import numpy as np
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics.pairwise import cosine_similarity
+
 import os
 
 app = Flask(__name__)
@@ -31,7 +29,7 @@ def get_audio_features(track_ids, access_token):
     #cause spotify only allows 100 ids at a time
     for i in range(0, len(track_ids), 100):
         batch = track_ids[i:i+100]
-        url = f"{SPOTIFY_API_BASE}/audio-features?ids={','.join(batch)}"
+        url = f"{SPOTIFY_API_URL}/audio-features?ids={','.join(batch)}"
         # join batch of ids with commas
         #get the features
         response = requests.get(url, headers=headers)
